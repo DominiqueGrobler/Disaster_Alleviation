0eb636d8eb061bb642fb96854646a1f4e273bc2b
Dominique Grobler <andrew@holiday>
2022-11-09 11:15:43 +0200

    Goods donation total

--- Disaster_Alleviation/Controllers/HomeController.cs
+++ Disaster_Alleviation/Controllers/HomeController.cs
@@ -15,14 +15,15 @@ namespace Disaster_Alleviation.Controllers
     {
         private readonly Monetary_donations_Context _Mcontext;
         private readonly Purchase_Context _Pcontext;
+        private readonly Goods_donation_Context _Gcontext;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger, Monetary_donations_Context Mcontext, Purchase_Context  Pcontext)
+        public HomeController(ILogger<HomeController> logger, Monetary_donations_Context Mcontext, Purchase_Context  Pcontext, Goods_donation_Context Gcontext)
         {
             _logger = logger;
             _Mcontext = Mcontext;
             _Pcontext = Pcontext;
-
+            _Gcontext = Gcontext;
 
         }
 
@@ -34,6 +35,12 @@ namespace Disaster_Alleviation.Controllers
             var total = _Mcontext.Monetary_donations.Where(x => x.Amount >= 0).Sum(y => y.Amount);
             ViewBag.Total = total;
             ViewBag.Left = total - purchaseTotal;
+
+            var goodsTotal = _Gcontext.Goods_donations.Where(x => x.Num_items >= 0).Sum(y => y.Num_items);
+            ViewBag.goodsTotal = goodsTotal;
+            var purchaseGoods= _Pcontext.Purchase.Where(x => x.Num_items >= 0).Sum(y => y.Num_items);
+            ViewBag.purchaseGoods = purchaseGoods;
+            ViewBag.goods= goodsTotal + purchaseGoods;
 
             HttpContext.Session.SetString("Test", "Ben Rules");
             ViewBag.Name = "Jess";
